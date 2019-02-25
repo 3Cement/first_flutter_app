16e0048950f0d3d2292850d1606217d175d2de13
3Cement <danielmilewski123@gmail.com>
2019-02-25 20:12:29 +0100

    added alert connected with delete button

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,37 +8,64 @@ class ProductPage extends StatelessWidget {
 
   ProductPage(this.title, this.imageUrl);
 
+  _showWarningDialog(BuildContext context) {
+    showDialog(
+                      context: context,
+                      builder: (BuildContext) {
+                        return AlertDialog(
+                          title: Text('Are you sure?'),
+                          content: Text('This action cannot be undode'),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text('DISCARD'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                            FlatButton(
+                              child: Text('CONTINUE'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                                Navigator.pop(context, true);
+                              },
+                            ),
+                          ],
+                        );
+                      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () {
-      print('Back button pressed!');
-      Navigator.pop(context, false);
-      return Future.value(false);
-    }, 
-    child: Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(title),
-          ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              color: Theme.of(context).accentColor,
-              child: Text('DELETE'), 
-              onPressed: () => Navigator.pop(context, true),
+        print('Back button pressed!');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(title),
             ),
-          )
-        ],
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('DELETE'),
+                onPressed: () => _showWarningDialog(context),
+              ),
+            )
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
