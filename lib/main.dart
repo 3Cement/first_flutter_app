c852080ff66808cc81ef176d084304b653396dd6
3Cement <danielmilewski123@gmail.com>
2019-03-25 20:05:15 +0100

    Details view improved

--- lib/main.dart
+++ lib/main.dart
@@ -41,10 +41,10 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       // debugShowMaterialGrid: true,
       theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple,
-          ),
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple,
+      ),
       // home: AuthPage(),
       routes: {
         '/': (BuildContext context) => AuthPage(),
@@ -61,7 +61,10 @@ class _MyAppState extends State<MyApp> {
           final int index = int.parse(pathElements[2]);
           return MaterialPageRoute<bool>(
             builder: (BuildContext context) => ProductPage(
-                _products[index]['title'], _products[index]['image']),
+                _products[index]['title'],
+                _products[index]['image'],
+                _products[index]['price'],
+                _products[index]['description']),
           );
         }
         return null;
--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -5,34 +5,10 @@ import 'dart:async';
 class ProductPage extends StatelessWidget {
   final String title;
   final String imageUrl;
+  final double price;
+  final String description;
 
-  ProductPage(this.title, this.imageUrl);
-
-  _showWarningDialog(BuildContext context) {
-    showDialog(
-                      context: context,
-                      builder: (BuildContext) {
-                        return AlertDialog(
-                          title: Text('Are you sure?'),
-                          content: Text('This action cannot be undode'),
-                          actions: <Widget>[
-                            FlatButton(
-                              child: Text('DISCARD'),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                            FlatButton(
-                              child: Text('CONTINUE'),
-                              onPressed: () {
-                                Navigator.pop(context);
-                                Navigator.pop(context, true);
-                              },
-                            ),
-                          ],
-                        );
-                      });
-  }
+  ProductPage(this.title, this.imageUrl, this.price, this.description);
 
   @override
   Widget build(BuildContext context) {
@@ -53,14 +29,37 @@ class ProductPage extends StatelessWidget {
             Image.asset(imageUrl),
             Container(
               padding: EdgeInsets.all(10.0),
-              child: Text(title),
+              child: Text(
+                title,
+                style: TextStyle(
+                    fontSize: 26.0,
+                    fontFamily: 'Oswald',
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Unin Square, San Francisco',
+                    style: TextStyle(fontFamily: 'Oswald', color: Colors.grey)),
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Text(
+                      '|',
+                      style:
+                          TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+                    )),
+                Text(
+                  '\$' + price.toString(),
+                  style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+                )
+              ],
             ),
             Container(
               padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('DELETE'),
-                onPressed: () => _showWarningDialog(context),
+              child: Text(
+                'fhoisajnhidfjsa fdjaisojdiowq iofjkapsojfpasj joipfjkaojfposa jhfiosjaifodpjsa',
+                textAlign: TextAlign.center,
               ),
             )
           ],
